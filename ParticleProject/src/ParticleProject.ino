30004c10f9914e4d2df2155b4bd38180dd4039da
Wazqaz <etienne.deshaies-samson@usherbrooke.ca>
2021-05-20 18:56:45 -0400

    Added client server request

--- ParticleProject/src/ParticleProject.ino
+++ ParticleProject/src/ParticleProject.ino
@@ -5,6 +5,9 @@
  * Date:
  */
 
+TCPClient client;
+byte server[] = { 192, 168, 0, 106 };
+
 int i = 0;
 
 // setup() runs once, when the device is first turned on.
@@ -21,4 +24,41 @@ void loop() {
   // The core of your code will likely live here.
   Serial.printlnf("Counting: %d", i++);
   delay(1000);
+
+  // POST Message
+  if(client.connect(server, 3000))
+  {
+    // Print some information that we have connected to the server
+    Serial.println("**********************************!");
+    Serial.println("New POST Connection!");
+    Serial.println("Connection OK!");
+            
+    // Send our HTTP data!
+    // client.println("POST /submit.php HTTP/1.0");
+    // client.println("Host: maker-io-iot.atwebpages.com");
+    // client.println("Content-Type: application/x-www-form-urlencoded");
+    // client.println("Content-Length: 9");
+    // client.println();
+    // client.print("number=10");
+
+    // Read data from the buffer
+    
+    // while(receivedData.indexOf("\r\n\r\n") == -1)
+    // {
+    //     memset(dataBuffer, 0x00, sizeof(dataBuffer));
+    //     client.read(dataBuffer, sizeof(dataBuffer));
+    //     receivedData += (const char*)dataBuffer;
+    // }
+    
+    // Print the string
+    //Serial.println(receivedData);
+
+    // Stop the current connection
+
+    client.stop();
+  }  
+  else
+  {
+      Serial.println("Server connection failed. Trying again...");
+  } 
 }
