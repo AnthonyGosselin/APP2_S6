925008e091859d691eebdf6d052d98ec29beb5c3
Wazqaz <etienne.deshaies-samson@usherbrooke.ca>
2021-05-20 19:35:53 -0400

    Working post to server in seerverJay, added Express

--- ParticleProject/src/ParticleProject.ino
+++ ParticleProject/src/ParticleProject.ino
@@ -8,7 +8,7 @@
 TCPClient client;
 byte server[] = { 192, 168, 0, 106 };
 
-int i = 0;
+//int i = 0;
 
 // setup() runs once, when the device is first turned on.
 void setup() {
@@ -22,8 +22,8 @@ void setup() {
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
   // The core of your code will likely live here.
-  Serial.printlnf("Counting: %d", i++);
-  delay(1000);
+  //Serial.printlnf("Counting: %d", i++);
+  delay(100);
 
   // POST Message
   if(client.connect(server, 3000))
@@ -32,14 +32,17 @@ void loop() {
     Serial.println("**********************************!");
     Serial.println("New POST Connection!");
     Serial.println("Connection OK!");
+
+    char* postVal = "{\"windSpeed\":\"420\"}";
             
     // Send our HTTP data!
-    // client.println("POST /submit.php HTTP/1.0");
-    // client.println("Host: maker-io-iot.atwebpages.com");
-    // client.println("Content-Type: application/x-www-form-urlencoded");
-    // client.println("Content-Length: 9");
-    // client.println();
-    // client.print("number=10");
+    client.println("POST /json HTTP/1.0");
+    client.println("Host: 192.168.0.106:3000");
+    client.println("Content-Type: application/json");
+    client.print("Content-Length: ");
+    client.println(strlen(postVal));
+    client.println();
+    client.print(postVal);
 
     // Read data from the buffer
     
@@ -54,7 +57,6 @@ void loop() {
     //Serial.println(receivedData);
 
     // Stop the current connection
-
     client.stop();
   }  
   else
