e31eb2813e8f16d5174328221737505ef25e4cca
AnthonyGosselin <gosa2506@usherbrooke.ca>
2021-05-20 18:55:05 -0400

    Working server v1

--- WebServer/app.js
+++ WebServer/app.js
@@ -4,8 +4,30 @@ const fs = require('fs')
 const hostname = '127.0.0.1';
 const port = 3000;
 
+// CURRENT VALUES 
 var currentWindSpeed = -1
+var currentTemperature = -1
 
+// Html content with most recent values
+function newHtmlContent() {
+    return `
+    <!doctype html>
+    <html lang="en">
+
+    <head>
+        <title>Weather Station</title>
+    </head>
+
+    <body>
+        <h1>Wind speed: ${currentWindSpeed}</h1>
+        <h1>Temperature: ${currentTemperature}</h1>
+    </body>
+
+    </html>
+    `
+}
+
+// Configure server responses
 const server = http.createServer((req, res) => {
     if (req.method === 'POST') {
         console.log("Received POST")
@@ -18,50 +40,24 @@ const server = http.createServer((req, res) => {
             var jsonData = JSON.parse(jsonString)
 
             var windSpeed = jsonData.windSpeed
+            var temperature = jsonData.temperature
             console.log('Wind speed: \{windSpeed}')
+
             currentWindSpeed = windSpeed
+            currentTemperature = temperature
         })
     }
     if (req.method === "GET") {
         console.log("Received GET")
     }
 
+    // Render page
     res.writeHead(200, {'Content-Type': 'text/html'});
-
-    // Write html file
-    fs.readFile('./WebServer/index.html', null, function(error, data) {
-        console.log("Get index")
-        if (error) {
-            console.log(error)
-            res.writeHead(404)
-            res.end()
-        } 
-        else {
-            //res.write(data)
-            //document.getElementById('windspeed').innerHTML = currentWindSpeed
-
-            
-            res.write(`
-                    <!doctype html>
-                    <html lang="en">
-
-                    <head>
-                        <title>Hello, world2!</title>
-                    </head>
-
-                    <body>
-                        <h1>Wind speed:\{currentWindSpeed}</h1>
-                    </body>
-
-                    </html>
-            `)
-            res.end()
-        }
-    })
-
-    var returnResult = "Current wind speed: \{currentWindSpeed}"
+    res.write(newHtmlContent())
+    res.end()
 });
 
+// Start listening
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
